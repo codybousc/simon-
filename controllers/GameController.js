dfdec57b4774be26a043eb636a96c2a678b79408
Cody Bouscaren <codybousc@gmail.com>
2016-01-28 15:54:10 -0500

    Button sound playback with delay functioning

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -28,29 +28,50 @@ $scope.compareArrays = function() {
   }
 }
 
-$scope.playBack = function(selection) {
-  console.log(selection);
+$scope.playerButtonPress = function(selection) {
+  if(selection == 'topLeft') {
+    topLeftAudio.play();
+  }
+  else if(selection == 'topRight') {
+    topRightAudio.play();
+  }
+  else if(selection == 'bottomLeft') {
+    bottomLeftAudio.play();
+  }
+  else if(selection == 'bottomRight') {
+    bottomRightAudio.play();
+  }
+}
+
+$scope.playBack = function(passedArray) {
   var topLeftAudio = document.getElementById("topLeftAudio");
   var topRightAudio = document.getElementById("topRightAudio");
   var bottomLeftAudio = document.getElementById("bottomLeftAudio");
   var bottomRightAudio = document.getElementById("bottomRightAudio");
-
-    if(selection == 'topLeft') {
-      topLeftAudio.play();
-    }
-    else if(selection == 'topRight') {
-      topRightAudio.play();
-    }
-    else if(selection == 'bottomLeft') {
-      bottomLeftAudio.play();
-    }
-    else if(selection == 'bottomRight') {
-      bottomRightAudio.play(); 
-    }
+  var i = 0, l = passedArray.length;
+    (function iterator() {
+      if(passedArray[i] == 1) {
+        topLeftAudio.play();
+      }
+      else if(passedArray[i] == 2) {
+        topRightAudio.play();
+      }
+      else if(passedArray[i] == 3) {
+        bottomLeftAudio.play();
+      }
+      else if(passedArray[i] == 4) {
+        bottomRightAudio.play();
+      }
+      if(++i<l) {
+          setTimeout(iterator, 1000);
+      }
+      })();
 
 }
 
+var arr = [1, 2, 3];
 
+$scope.playBack(arr);
 
 
 
