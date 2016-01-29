b519ffa2824c063751c6b38d2c79e5c779afbdb7
Cody Bouscaren <codybousc@gmail.com>
2016-01-29 10:50:01 -0500

    Basic game functionality working

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -6,28 +6,49 @@ function($scope, $http) {
 $scope.buttonChoices = [1, 2, 3, 4];
 $scope.roundArray = [];
 $scope.playerArray = [];
-$scope.arraysAreEqual = true;
+$scope.arraysAreEqual = false;
 
+//populates first index of roundArray on init and subsequent iterations on correct answer submission
+//(found on $scope.arraysAreEqual boolean)
 $scope.buttonSelection = function() {
   var randomSelection = Math.floor((Math.random() * 4) + 1);
   $scope.roundArray.push(randomSelection);
-  console.log("RoundArray = ", $scope.roundArray);
+  console.log("Line 16 RoundArray = ", $scope.roundArray);
 }
 
+//populates playerArray and calls compareArrays function
 $scope.playerSelection = function(selection) {
   $scope.playerArray.push(selection);
-  console.log("playerArray = ", $scope.playerArray);
+  console.log("playerSelection Function playerArray = ", $scope.playerArray);
+  console.log("=========================================================");
+  if($scope.playerArray.length == $scope.roundArray.length) {
+    console.log("SAME LENGTH ");
+    console.log("playerSelection Function playerArray = ", $scope.playerArray);
+    console.log("playerSelection Function roundArray = ", $scope.roundArray);
+    $scope.compareArrays();
+  }
 }
 
+//compares player and round arrays and sets arraysAreEqual variable (boolean)
 $scope.compareArrays = function() {
+  console.log("Making it to compareArrays");
+  roundArray = $scope.roundArray;
   for(var i = 0; i < roundArray.length; i++) {
-    if($scope.playerArray[i] != $scope.roundArray[i]) {
+    console.log("$scope.playerArray[i] = ",  $scope.playerArray[i] );
+    console.log("roundArray[i] = ",  roundArray[i] )
+
+    if($scope.playerArray[i] == roundArray[i]) {
+      $scope.arraysAreEqual = true;
+    }
+    else if($scope.playerArray[i] != roundArray[i]) {
       $scope.arraysAreEqual = false;
       break;
     }
   }
+  console.log("line 37, $scope.arraysAreEqual ", $scope.arraysAreEqual);
 }
 
+//plays sounds on player button press
 $scope.playerButtonPress = function(selection) {
   if(selection == 'topLeft') {
     topLeftAudio.play();
@@ -43,37 +64,54 @@ $scope.playerButtonPress = function(selection) {
   }
 }
 
+//TODO Need to make the buttons light up on playerButtonPress and playback
+//Iterates through roundArray and plays back populated sounds
 $scope.playBack = function(passedArray) {
   var topLeftAudio = document.getElementById("topLeftAudio");
   var topRightAudio = document.getElementById("topRightAudio");
   var bottomLeftAudio = document.getElementById("bottomLeftAudio");
   var bottomRightAudio = document.getElementById("bottomRightAudio");
   var i = 0, l = passedArray.length;
-    (function iterator() {
-      if(passedArray[i] == 1) {
-        topLeftAudio.play();
-      }
-      else if(passedArray[i] == 2) {
-        topRightAudio.play();
-      }
-      else if(passedArray[i] == 3) {
-        bottomLeftAudio.play();
-      }
-      else if(passedArray[i] == 4) {
-        bottomRightAudio.play();
-      }
-      if(++i<l) {
-          setTimeout(iterator, 1000);
-      }
-      })();
-
+  (function iterator() {
+    if(passedArray[i] == 1) {
+      topLeftAudio.play();
+    }
+    else if(passedArray[i] == 2) {
+      topRightAudio.play();
+    }
+    else if(passedArray[i] == 4) {
+      bottomLeftAudio.play();
+    }
+    else if(passedArray[i] == 3) {
+      bottomRightAudio.play();
+    }
+    if(++i<l) {
+        setTimeout(iterator, 1000);
+    }
+    })();
 }
 
-var arr = [1, 2, 3];
+$scope.gamePlayer = function() {
+  var roundArray = $scope.roundArray;
+  console.log("line 80 arraysAreEqual ", $scope.arraysAreEqual);
+  if($scope.playerArray.length == roundArray.length && $scope.arraysAreEqual) {
+    console.log("Correct selection");
+    console.log("============================================")
+    $scope.buttonSelection();
 
-$scope.playBack(arr);
+     setTimeout(function() {
+       $scope.playBack(roundArray);
+    }, 2000);
 
-
-
+    //Reset playerArray
+    $scope.playerArray = [];
+  }
+  else if(!$scope.arraysAreEqual) {
+    console.log("Incorrect selection");
+    setTimeout(function() {
+      $scope.playBack(roundArray);
+   }, 2000);
+  }
+}
 
 }]);
